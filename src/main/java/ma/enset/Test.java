eee73d6505ff8c0773bb2fe3c6ebfe35eaf6b826
bambiste <khadimtraore8@gmail.com>
2021-11-22 16:29:39 +0000

    firts commit branch dev

--- src/main/java/ma/enset/Test.java
+++ src/main/java/ma/enset/Test.java
@@ -1,6 +1,9 @@
 package main.java.ma.enset;
-
+/** 
+ * Premier Commentaire de la classe Test
+*/
 public class Test {
+
     public static void main(String[] args) {
         System.out.print("Hello world");
     }
